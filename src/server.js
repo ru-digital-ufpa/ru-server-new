044e5b988fa2763a29227b92000d11f3ca237012
prinako <nyarkonton122@gmail.com.com>
2023-01-08 00:31:58 -0300

    hi

--- src/server.js
+++ src/server.js
@@ -10,6 +10,7 @@ app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(bodyParser.json());
+app.set('json spaces', 40);
 
 const port = process.env.PORT || 5500;
 
